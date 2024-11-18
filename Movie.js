b9fe24634bef42f555682e0ff964773f5855c842
Saramegomes <sara.melissa.andrade@escola.pr.gov.br>
2024-11-18 20:53:21 -0300

    Create Movie.js

---
+++ Movie.js
@@ -0,0 +1,156 @@
+MOVIES.JS 
+export const movies = [ { 
+title: "A Fantástica Fábrica de Chocolate", 
+genre: "suspense ", 
+
+ageRating: livre, 
+synopsis: " Um jovem menino e outros cinco concorrentes ganham um passeio pela famosa fábrica de chocolate de Willy Wonka, onde coisas estranhas e misteriosas começam a acontecer.
+", 
+trailer: " https://youtu.be/beurNg4DGm0?si=f8Xz48mTapE6nUXE “
+}, 
+{ 
+title: "A Vila", 
+genre: "suspense", 
+ageRating: 10, 
+synopsis: " Em uma comunidade isolada, os moradores vivem com medo de criaturas que habitam a floresta ao redor. Quando um jovem ousa desafiar as regras, segredos são revelados.", 
+trailer: " https://youtu.be/MnvM9qlZXsg?si=SRbep8s9APzEKqmq "
+}, 
+{ 
+title: "Os Outros", 
+genre:"suspense", 
+ageRating: 12, 
+synopsis: " Uma mulher com dois filhos, que têm uma condição rara que os faz ser sensíveis à luz, vive em uma mansão isolada e começa a acreditar que está sendo assombrada por espíritos.
+", 
+trailer: " https://youtu.be/iZmV5gOwHRI?si=qI1kSU-GCUeNH1ye " 
+}, 
+{ 
+title: "Garota Exemplar" ", 
+genre: "suspense", 
+ageRating: 16, 
+synopsis: "O filme oferece uma trama cheia de reviravoltas e elementos de mistério psicológico, com temas que são mais intensos e complexos, adequados para maiores de 16 anos.
+", 
+trailer: " https://youtu.be/HXV3dipeWJA?si=xlTYRM6BKJHrxuA0 " 
+}, 
+{ 
+title: "Seven - Os Sete Crimes Capitais", 
+genre: "suspense", 
+ageRating: 18, 
+synopsis: ""Seven" é um thriller psicológico tenso e perturbador, com violência gráfica e temas maduros, ideal para maiores de 18 anos.
+", 
+trailer: " https://youtu.be/JXgOWdG5wqU?si=9F2iwHrNIVrSKwve “
+
+}, 
+{ 
+title: "O Menino e o Mundo", 
+genre:drama "suspense", 
+ageRating: livre, 
+synopsis: " Uma animação brasileira que conta a história de um menino que sai em uma jornada em busca de seu pai. O filme explora temas como a infância, a pobreza e a natureza humana, usando um estilo de animação vibrante e simbólico. É uma obra sensível e poética, indicada para todas as idades.
+
+", 
+trailer: " https://youtu.be/l7x8oi_1GBo?si=-BaJeHt_nxZK14hp “
+}, 
+{ 
+title: "O Pequeno Príncipe",
+genre: "drama", 
+ageRating: 10, synopsis: " Baseado na obra clássica de Antoine de Saint-Exupéry, o filme mistura animação e live-action, contando a história de uma menina que, sob a orientação de um excêntrico aviador, redescobre a infância e os valores da vida através do "Pequeno Príncipe". Um filme com uma forte mensagem emocional e reflexiva sobre a importância da imaginação e das relações humanas.
+", 
+trailer: “ https://youtu.be/7WoO-luLshk?si=gAvpZ1B9SE6FWR75 “ 
+}, 
+{ 
+title: "O Som do Coração", 
+genre: "drama", 
+ageRating: 12, synopsis: "A história de um menino prodígio da música que perde a audição em um acidente. Ele luta para manter sua paixão pela música e enfrentar os desafios da vida. O filme transmite mensagens de superação, amizade e resiliência, além de abordar a importância da arte e da expressão pessoal.
+", 
+trailer: " https://youtu.be/Emt6GZwF_ec?si=Aya7LiEkpneTARRF “
+}, 
+{ 
+title: "A Culpa é das Estrelas", 
+genre: "", 
+ageRating: 16, synopsis: "Baseado no livro de John Green, o filme acompanha a história de Hazel e Gus, dois adolescentes que se apaixonam enquanto lidam com a luta contra o câncer. A história aborda temas de amor, perda e o sentido da vida, com uma abordagem sensível e emocional sobre a fragilidade da existência.
+", 
+trailer: " https://youtu.be/lFOOZJ1UChg?si=UaO9uK7XKze4MC7n "  
+}, 
+{ 
+title: "Requiem for a Dream", 
+genre: "drama", 
+ageRating: 18, synopsis: "Dirigido por Darren Aronofsky, o filme narra a vida de quatro personagens que se deixam consumir por vícios e sonhos irreais. A obra é uma análise perturbadora das consequências do abuso de substâncias e do desespero humano. Um drama psicológico intenso, com cenas gráficas e um retrato brutal da decadência emocional e física dos personagens.
+", 
+trailer: " https://youtu.be/kJ39qXjKLWs?si=rihSBv4bNAjsLu15 " 
+}, export const movies = [ { 
+title: "A Fantástica Fábrica de Chocolate", 
+genre: "suspense ", 
+
+ageRating: livre, 
+synopsis: " Um jovem menino e outros cinco concorrentes ganham um passeio pela famosa fábrica de chocolate de Willy Wonka, onde coisas estranhas e misteriosas começam a acontecer.
+", 
+trailer: "https://youtu.be/beurNg4DGm0?si=f8Xz48mTapE6nUXE" 
+}, 
+{ 
+title: "A Vila", 
+genre: "suspense", 
+ageRating: 10, 
+synopsis: " Em uma comunidade isolada, os moradores vivem com medo de criaturas que habitam a floresta ao redor. Quando um jovem ousa desafiar as regras, segredos são revelados.", 
+trailer: "https://youtu.be/MnvM9qlZXsg?si=SRbep8s9APzEKqmq" 
+}, 
+{ 
+title: "Os Outros", 
+genre:"suspense", 
+ageRating: 12, 
+synopsis: " Uma mulher com dois filhos, que têm uma condição rara que os faz ser sensíveis à luz, vive em uma mansão isolada e começa a acreditar que está sendo assombrada por espíritos.
+", 
+trailer: "https://youtu.be/iZmV5gOwHRI?si=qI1kSU-GCUeNH1ye" 
+}, 
+{ 
+title: "Garota Exemplar" ", 
+genre: "suspense", 
+ageRating: 16, 
+synopsis: "O filme oferece uma trama cheia de reviravoltas e elementos de mistério psicológico, com temas que são mais intensos e complexos, adequados para maiores de 16 anos.
+", 
+trailer: "https://youtu.be/HXV3dipeWJA?si=xlTYRM6BKJHrxuA0" 
+}, 
+{ 
+title: "Seven - Os Sete Crimes Capitais", 
+genre: "suspense", 
+ageRating: 18, 
+synopsis: ""Seven" é um thriller psicológico tenso e perturbador, com violência gráfica e temas maduros, ideal para maiores de 18 anos.
+", 
+trailer: "https://youtu.be/JXgOWdG5wqU?si=9F2iwHrNIVrSKwve’
+
+}, 
+{ 
+title: "O Menino e o Mundo", 
+genre:drama "suspense", 
+ageRating: livre, 
+synopsis: " Uma animação brasileira que conta a história de um menino que sai em uma jornada em busca de seu pai. O filme explora temas como a infância, a pobreza e a natureza humana, usando um estilo de animação vibrante e simbólico. É uma obra sensível e poética, indicada para todas as idades.
+
+", 
+trailer: "https://youtu.be/l7x8oi_1GBo?si=-BaJeHt_nxZK14hp" 
+}, 
+{ 
+title: "O Pequeno Príncipe",
+genre: "drama", 
+ageRating: 10, synopsis: " Baseado na obra clássica de Antoine de Saint-Exupéry, o filme mistura animação e live-action, contando a história de uma menina que, sob a orientação de um excêntrico aviador, redescobre a infância e os valores da vida através do "Pequeno Príncipe". Um filme com uma forte mensagem emocional e reflexiva sobre a importância da imaginação e das relações humanas.
+", 
+trailer: "https://youtu.be/7WoO-luLshk?si=gAvpZ1B9SE6FWR75" 
+}, 
+{ 
+title: "O Som do Coração", 
+genre: "drama", 
+ageRating: 12, synopsis: "A história de um menino prodígio da música que perde a audição em um acidente. Ele luta para manter sua paixão pela música e enfrentar os desafios da vida. O filme transmite mensagens de superação, amizade e resiliência, além de abordar a importância da arte e da expressão pessoal.
+", 
+trailer: "https://youtu.be/Emt6GZwF_ec?si=Aya7LiEkpneTARRF" 
+}, 
+{ 
+title: "A Culpa é das Estrelas", 
+genre: "", 
+ageRating: 16, synopsis: "Baseado no livro de John Green, o filme acompanha a história de Hazel e Gus, dois adolescentes que se apaixonam enquanto lidam com a luta contra o câncer. A história aborda temas de amor, perda e o sentido da vida, com uma abordagem sensível e emocional sobre a fragilidade da existência.
+", 
+trailer: "https://youtu.be/lFOOZJ1UChg?si=UaO9uK7XKze4MC7n" 
+}, 
+{ 
+title: "Requiem for a Dream", 
+genre: "drama", 
+ageRating: 18, synopsis: "Dirigido por Darren Aronofsky, o filme narra a vida de quatro personagens que se deixam consumir por vícios e sonhos irreais. A obra é uma análise perturbadora das consequências do abuso de substâncias e do desespero humano. Um drama psicológico intenso, com cenas gráficas e um retrato brutal da decadência emocional e física dos personagens.
+", 
+trailer: "https://youtu.be/kJ39qXjKLWs?si=rihSBv4bNAjsLu15" 
+
